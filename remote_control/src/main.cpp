ff4c2da62dda45be08f4b8b3077febf0615dbb1a
Carlos Giron <carlosegiron92@gmail.com>
2026-05-01 03:20:10 -0400

    added joystick code changes

---
+++ remote_control/src/main.cpp
@@ -0,0 +1,24 @@
+
+// Use for testing joy stick
+
+#include <Arduino.h>
+#include "config.h"
+#include "display.h"
+#include "joystick.h"
+
+void setup() {
+    Serial.begin(115200);
+    delay(500);
+    Serial.println("Beginning display init");
+    display_init();
+    Serial.println("display done");
+    joystick_init();
+    Serial.println("Joystick init done");
+    Serial.println("Joystick test ready. Move the stick and watch.");
+}
+
+void loop() {
+    joystick_debug_print();
+    Serial.println("hello");
+    delay(100);
+}
